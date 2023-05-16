e04ef731cad63380dbc8a56dcbc31f6da9211b75
Ahmed Emad <50369848+ahmedemad242@users.noreply.github.com>
2023-05-17 02:08:22 +0300

    feat: add controller and service logic

--- openfabric-api/src/main/java/ai/openfabric/api/repository/WorkerRepository.java
+++ openfabric-api/src/main/java/ai/openfabric/api/repository/WorkerRepository.java
@@ -1,8 +1,6 @@
 package ai.openfabric.api.repository;
 
 import ai.openfabric.api.model.Worker;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WorkerRepository extends CrudRepository<Worker, String> {
-
-}
+public interface WorkerRepository extends JpaRepository<Worker, String> { }
